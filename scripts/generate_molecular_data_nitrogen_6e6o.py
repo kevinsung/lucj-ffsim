fix nitrogen calculation

scripts/generate_molecular_data_nitrogen_6e6o.py
@@ -10,7 +10,7 @@ DATA_ROOT = "/disk1/kevinsung@ibm.com/lucj-ffsim"
 
 DATA_DIR = os.path.join(DATA_ROOT, "molecular_data")
 BASIS = "sto-6g"
-NE, NORB = 10, 8
+NE, NORB = 6, 6
 BASE_NAME = f"nitrogen_dissociation_{BASIS}_{NE}e{NORB}o"
 
 
@@ -27,7 +27,7 @@ def find_twop_shell():
     mf_ATOM = scf.ROHF(ATOM)
     mf_ATOM = scf.newton(mf_ATOM)
     mf_ATOM.kernel()
-    DL = mf_ATOM.mo_coeff[:, [1, 2, 3, 4]]
+    DL = mf_ATOM.mo_coeff[:, [2, 3, 4]]
     return DL
 
 
@@ -70,7 +70,9 @@ t2 = None
 t1_as = None
 t2_as = None
 
-for j, d in enumerate(list(np.arange(0.80, 3.01, 0.10))):
+d_range = np.arange(0.80, 3.01, 0.10)
+# d_range = np.arange(0.80, 1.11, 0.10)
+for j, d in enumerate(d_range):
     filename = os.path.join(DATA_DIR, f"{BASE_NAME}_d-{d:.2f}.pickle")
 
     mol = gto.Mole()
@@ -110,18 +112,22 @@ for j, d in enumerate(list(np.arange(0.80, 3.01, 0.10))):
     t2 = np.einsum("ijab,pa,qb,sj,ri->prqs", mcc.t2, Cv, Cv, Co, Co, optimize=True)
 
     nao = mol.nao_nr()
-    D = np.zeros((nao, 8))
-    D[: nao // 2, :4] = C
-    D[nao // 2 :, 4:] = C
+    D = np.zeros((nao, 6))
+    D[: nao // 2, :3] = C
+    D[nao // 2 :, 3:] = C
 
     PROJ, IDX = get_projections(mf, get_MP2_natural_orbitals(mf), D)
     print("bondlength, projections ", d, PROJ[IDX], IDX)
 
-    norb = 8
-    nelec = 5, 5
+    norb = 6
+    nelec = 3, 3
     mycas = mcscf.CASCI(mf, norb, nelec)
     mycas.mo_coeff = get_MP2_natural_orbitals(mf)
-    mo = mycas.sort_mo(IDX, base=0)
+    # mycas.mo_coeff = mycas.sort_mo(IDX, base=0)
+    if d > 2.00:
+        mo = mycas.sort_mo(IDX, base=0)
+    else:
+        mo = None
     mycas.fix_spin_(ss=0)
     Ecas = mycas.kernel(mo)[0]
 

scripts/plot_nitrogen_6e6o.py
@@ -18,10 +18,11 @@ os.makedirs(PLOTS_DIR, exist_ok=True)
 
 
 basis = "sto-6g"
-ne, norb = 10, 8
+ne, norb = 6, 6
 molecule_basename = f"nitrogen_dissociation_{basis}_{ne}e{norb}o"
 
 reference_curves_d_range = np.arange(0.80, 3.01, 0.10)
+# reference_curves_d_range = np.arange(0.80, 1.11, 0.10)
 # bond_distance_range = np.linspace(1.3, 4.0, 20)
 connectivities = [
     "square",
@@ -52,7 +53,7 @@ mol_datas_reference: dict[float, ffsim.MolecularData] = {}
 mol_datas_experiment: dict[float, ffsim.MolecularData] = {}
 
 for d in reference_curves_d_range:
-    filename = os.path.join(MOL_DATA_DIR, f"{molecule_basename}_d_{d:.2f}.pickle")
+    filename = os.path.join(MOL_DATA_DIR, f"{molecule_basename}_d-{d:.2f}.pickle")
     with open(filename, "rb") as f:
         mol_data = pickle.load(f)
         mol_datas_reference[d] = mol_data
@@ -157,7 +158,7 @@ for connectivity, n_reps in itertools.product(connectivities, n_reps_range):
     # plot_optimization_iterations(connectivity=connectivity, n_reps=n_reps)
     plot_reference_curves(
         plots_dir=PLOTS_DIR,
-        title="Nitrogen dissociation STO-6g (10e, 8o)",
+        title="Nitrogen dissociation STO-6g (6e, 6o)",
         molecule_basename=molecule_basename,
         reference_curves_bond_distance_range=reference_curves_d_range,
         hf_energies_reference=hf_energies_reference,

scripts/run_nitrogen_10e8o.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S %z",
-    filename="logs/run_nitrogen.log",
+    filename=f"logs/{os.path.splitext(os.path.basename(__file__))[0]}.log",
 )
 
 DATA_ROOT = "/disk1/kevinsung@ibm.com/lucj-ffsim"
@@ -29,19 +29,19 @@ overwrite = True
 
 d_range = np.arange(0.80, 3.01, 0.10)
 connectivities = [
-    "all-to-all",
     "square",
+    "all-to-all",
 ]
 n_reps_range = [
     2,
     4,
     6,
 ]
-with_final_orbital_rotation_choices = [False]
 optimization_methods = [
     "L-BFGS-B",
     "linear-method",
 ]
+with_final_orbital_rotation_choices = [False]
 maxiter = 1000
 
 tasks = [
@@ -57,13 +57,13 @@ tasks = [
     for (
         connectivity,
         n_reps,
-        with_final_orbital_rotation,
         optimization_method,
+        with_final_orbital_rotation,
     ) in itertools.product(
         connectivities,
         n_reps_range,
-        with_final_orbital_rotation_choices,
         optimization_methods,
+        with_final_orbital_rotation_choices,
     )
     for d in d_range
 ]

scripts/run_nitrogen_6e6o.py
@@ -20,14 +20,15 @@ DATA_ROOT = "/disk1/kevinsung@ibm.com/lucj-ffsim"
 
 MOL_DATA_DIR = os.path.join(DATA_ROOT, "molecular_data")
 DATA_DIR = os.path.join(DATA_ROOT, "lucj")
-MAX_PROCESSES = 144
+MAX_PROCESSES = 1
 
 basis = "sto-6g"
-ne, norb = 4, 4
-molecule_basename = f"ethene_dissociation_{basis}_{ne}e{norb}o"
+ne, norb = 6, 6
+molecule_basename = f"nitrogen_dissociation_{basis}_{ne}e{norb}o"
 overwrite = True
 
-bond_distance_range = np.linspace(1.3, 4.0, 20)
+d_range = np.arange(0.80, 3.01, 0.10)
+
 connectivities = [
     "square",
     "all-to-all",
@@ -37,16 +38,16 @@ n_reps_range = [
     4,
     6,
 ]
-with_final_orbital_rotation_choices = [False]
 optimization_methods = [
     "L-BFGS-B",
     "linear-method",
 ]
-maxiter = 10000
+with_final_orbital_rotation_choices = [False]
+maxiter = 1000
 
 tasks = [
     LUCJTask(
-        molecule_basename=f"{molecule_basename}_bond_distance_{bond_distance:.5f}",
+        molecule_basename=f"{molecule_basename}_d-{d:.2f}",
         connectivity=connectivity,
         n_reps=n_reps,
         with_final_orbital_rotation=with_final_orbital_rotation,
@@ -57,15 +58,15 @@ tasks = [
     for (
         connectivity,
         n_reps,
-        with_final_orbital_rotation,
         optimization_method,
+        with_final_orbital_rotation,
     ) in itertools.product(
         connectivities,
         n_reps_range,
-        with_final_orbital_rotation_choices,
         optimization_methods,
+        with_final_orbital_rotation_choices,
     )
-    for bond_distance in bond_distance_range
+    for d in d_range
 ]
 
 with ProcessPoolExecutor(MAX_PROCESSES) as executor:
@@ -76,4 +77,4 @@ with ProcessPoolExecutor(MAX_PROCESSES) as executor:
             data_dir=DATA_DIR,
             mol_data_dir=MOL_DATA_DIR,
             overwrite=overwrite,
-        )
+        ).result()

src/lucj_ffsim/lucj.py
@@ -153,6 +153,7 @@ def run_lucj_task(
     if task.optimization_method == "L-BFGS-B":
 
         def callback(intermediate_result: scipy.optimize.OptimizeResult):
+            logging.info(f"Task {task} is on iteration {info['nit']}.")
             info["x"].append(intermediate_result.x)
             info["fun"].append(intermediate_result.fun)
             info["nit"] += 1
@@ -180,7 +181,7 @@ def run_lucj_task(
             if hasattr(intermediate_result, "variation"):
                 info["variation"].append(intermediate_result.variation)
             nit = info["nit"]
-            if nit < 100 or nit % 20 == 0:
+            if nit < 20 or nit % 50 == 0:
                 if hasattr(intermediate_result, "energy_mat"):
                     info["energy_mat"].append((nit, intermediate_result.energy_mat))
                 if hasattr(intermediate_result, "overlap_mat"):

src/lucj_ffsim/plot.py
@@ -111,7 +111,7 @@ def plot_reference_curves(
         label="FCI",
         color="black",
     )
-
+    ax.legend()
     ax.set_title(title)
 
     dirname = os.path.join(
@@ -264,15 +264,16 @@ def plot_overlap_mats(
             "linear-method",
         ]
 
-        overlap_mats = np.stack(info["overlap_mat"])
+        iterations, overlap_mats = zip(*info["overlap_mat"])
         nit = len(overlap_mats)
         step = nit // (n_mat - 1)
-        mats = list(overlap_mats[::step])
-        iteration_nums = list(range(0, nit, step))
-        assert len(mats) in (n_mat, n_mat - 1)
-        # if len(mats) == n_mat - 1:
-        #     mats.append(overlap_mats[-1])
-        #     iteration_nums.append(nit - 1)
+        indices = list(range(0, nit, step))
+        while len(indices) > n_mat - 1:
+            indices.pop()
+        if nit - 1 not in indices:
+            indices.append(nit - 1)
+        mats = [overlap_mats[i] for i in indices]
+        iteration_nums = [iterations[i] for i in indices]
 
         for ax, mat, i in zip(these_axes, mats, iteration_nums):
             max_val = np.max(np.abs(mat))